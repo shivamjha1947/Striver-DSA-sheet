74e9666e709da066aceaf0267ebaa163558fb6c8
shivam jha <shivam36_soe@jnu.ac.in>
2022-03-09 23:59:09 +0530

    Update sort 0s, 1s, 2s.cpp

--- Day 1/sort 0s, 1s, 2s.cpp
+++ Day 1/sort 0s, 1s, 2s.cpp
@@ -1,3 +1,4 @@
+// Method 1
 void sortColors(vector<int>& nums) {
     int zero=0, one=0, n=nums.size();
     for(int i=0;i<n;i++){
@@ -15,5 +16,16 @@ void sortColors(vector<int>& nums) {
     }
     for(int i=zero+one;i<n;i++){
         nums[i]=2;
+    }
+}
+
+// Method 2
+void sortColors(int A[], int n) {
+    int j = 0, k = n - 1;
+    for (int i = 0; i <= k; ++i){
+        if (A[i] == 0 && i != j)
+            swap(A[i--], A[j++]);
+        else if (A[i] == 2 && i != k)
+            swap(A[i--], A[k--]);
     }
 }
