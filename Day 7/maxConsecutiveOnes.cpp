cac656bf0182e2f631a63fbc0254838afb356c34
shivam jha <shivam36_soe@jnu.ac.in>
2022-04-29 22:38:42 +0530

    Create maxConsecutiveOnes.cpp

---
+++ Day 7/maxConsecutiveOnes.cpp
@@ -0,0 +1,14 @@
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int maxCount=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+                maxCount=max(maxCount,count);
+            }else{
+                count=0;
+            }
+            
+        }
+        return maxCount;
+    }
