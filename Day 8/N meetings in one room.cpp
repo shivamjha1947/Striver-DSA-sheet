948f9fa0e516ed60ea77190295332936c0f6d430
shivam jha <shivam36_soe@jnu.ac.in>
2022-04-30 15:17:52 +0530

    Create N meetings in one room.cpp

---
+++ Day 8/N meetings in one room.cpp
@@ -0,0 +1,28 @@
+bool static comp(pair<int,int> &a, pair<int,int> &b){
+        if(a.second<b.second){
+            return true;
+        }else if(a.second>=b.second){
+            return false;
+        }else if(a.first<b.first){
+            return true;
+        }
+        return false;
+    }
+    int maxMeetings(int s[], int e[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++){
+            v.push_back({s[i],e[i]});
+        }
+        sort(v.begin(),v.end(),comp);
+        int high=v[0].second;
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(v[i].first>high){
+                count++;
+                high=v[i].second;
+            }
+        }
+        return count;
+    }
