8a3a9107a4bc2da9cbec189748c1330b0fb9f13c
shivam jha <shivam36_soe@jnu.ac.in>
2022-06-13 23:14:30 +0530

    Create Next largest ele.cpp

---
+++ Day 13/Next largest ele.cpp
@@ -0,0 +1,28 @@
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        vector<int> ans;
+        unordered_map<int,int> m;
+        m[nums2[nums2.size()-1]]=-1;
+        s.push(nums2[nums2.size()-1]);
+        for(int i=nums2.size()-2;i>=0;i--){
+            if(s.empty()){
+                m[nums2[i]]=-1;
+            }else if(!s.empty() && s.top()>nums2[i]){
+                m[nums2[i]]=s.top();
+            }else if(!s.empty() && s.top()<nums2[i]){
+                while(!s.empty() && s.top()<nums2[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    m[nums2[i]]=-1;
+                }else{
+                    m[nums2[i]]=s.top();
+                }
+            }
+            s.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(m[nums1[i]]);
+        }
+        return ans;
+    }
