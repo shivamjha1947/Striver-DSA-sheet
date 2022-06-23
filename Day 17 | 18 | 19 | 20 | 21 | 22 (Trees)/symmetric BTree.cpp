f23467fc56f1091b01eb03a808828b6d02ac0f26
shivam jha <shivam36_soe@jnu.ac.in>
2022-06-23 21:06:42 +0530

    Create symmetric BTree.cpp

---
+++ Day 17 | 18 | 19 | 20 | 21 | 22 (Trees)/symmetric BTree.cpp
@@ -0,0 +1,18 @@
+bool isSymmetric(TreeNode *root) {
+        if (!root) return true;
+        return helper(root->left, root->right);
+    }
+    
+    bool helper(TreeNode* p, TreeNode* q) {
+        if (!p && !q) {
+            return true;
+        } else if (!p || !q) {
+            return false;
+        }
+        
+        if (p->val != q->val) {
+            return false;
+        }
+        
+        return helper(p->left,q->right) && helper(p->right, q->left); 
+    }
