30cfd39b11fd369b7ada767f652f6c7c7894ab2d
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-01 22:46:52 +0530

    Create flatten BTree.cpp

---
+++ Day 17 | 18 | 19 | 20 | 21 | 22 (Trees)/flatten BTree.cpp
@@ -0,0 +1,36 @@
+// Method 1) Recursion
+TreeNode * prev = NULL;
+    void flatten(TreeNode * root) {
+      if (root == NULL) return;
+
+      flatten(root -> right);
+      flatten(root -> left);
+
+      root -> right = prev;
+      root -> left = NULL;
+      prev = root;
+    }
+
+// Method 2) Stack same as above just reverse
+
+// Method 3) Morris traversal
+node * prev = NULL;
+  public:
+   void flatten(node* root) {
+        node* cur = root;
+		while (cur)
+		{
+			if(cur->left)
+			{
+				node* pre = cur->left;
+				while(pre->right)
+				{
+					pre = pre->right;
+				}
+				pre->right = cur->right;
+				cur->right = cur->left;
+				cur->left = NULL;
+			}
+			cur = cur->right;
+		}
+    }
