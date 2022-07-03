c120f50da8258481bc05767544ae5acf8f7ca889
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-03 17:35:58 +0530

    Create Kth smallest\largest ele in BST.cpp

---
+++ Day 20 | 21 | 22 (BST)/Kth smallest\largest ele in BST.cpp
@@ -0,0 +1,30 @@
+node* kthlargest(node* root,int& k)
+{
+	if(root==NULL)
+	return NULL;
+	
+	node* right=kthlargest(root->right,k);
+	if(right!=NULL)
+	return right;
+	k--;
+	
+	if(k==0)
+	return root;
+	
+	return kthlargest(root->left,k);
+}
+
+node* kthsmallest(node* root,int &k)
+{
+	if(root==NULL)
+	return NULL;
+	
+	node* left=kthsmallest(root->left,k);
+	if(left!=NULL)
+	return left;
+	k--;
+	if(k==0)
+	return root;
+	
+	return kthsmallest(root->right,k);
+}
