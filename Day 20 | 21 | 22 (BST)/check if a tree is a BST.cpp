f4c382d9d548af6aad04a95a308009d0179653ad
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-03 21:43:43 +0530

    Create check if a tree is a BST.cpp

---
+++ Day 20 | 21 | 22 (BST)/check if a tree is a BST.cpp
@@ -0,0 +1,13 @@
+bool isValidBST(TreeNode* root) {
+        return help(root, LONG_MIN, LONG_MAX);
+    }
+    
+    bool help(TreeNode* root, long min, long max){
+        if(!root){ 
+            return true;
+        }    
+        if(root->val <= min || root->val >= max)  {
+            return false;
+        }
+        return help(root->left,min,root->val) && help(root->right,root->val, max);
+    }
