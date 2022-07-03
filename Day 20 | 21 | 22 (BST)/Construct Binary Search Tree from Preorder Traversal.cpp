9bb80b1c2ae367d4d314cac31051c15223842d61
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-03 18:13:52 +0530

    Create Construct Binary Search Tree from Preorder Traversal.cpp

---
+++ Day 20 | 21 | 22 (BST)/Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,13 @@
+TreeNode* bstFromPreorder(vector<int>& A) {
+        int i = 0;
+        return build(A, i, INT_MAX);
+    }
+
+    TreeNode* build(vector<int>& A, int& i, int bound) {
+        if (i == A.size() || A[i] > bound) return NULL;
+        TreeNode* root = new TreeNode(A[i]);
+        i++;
+        root->left = build(A, i, root->val);
+        root->right = build(A, i, bound);
+        return root;
+    }
