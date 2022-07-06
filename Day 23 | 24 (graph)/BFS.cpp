afa5f7f3914bea307730d2d2941aa3b5bc3c554a
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-06 18:55:37 +0530

    Update BFS.cpp

--- Day 23 | 24 (graph)/BFS.cpp
+++ Day 23 | 24 (graph)/BFS.cpp
@@ -1,21 +1,24 @@
 vector < int > bfsOfGraph(int V, vector < int > adj[]) {
   vector < int > bfs;
   vector < int > vis(V, 0);
-  queue < int > q;
-  q.push(0);
-  vis[0] = 1;
-  while (!q.empty()) {
-    int node = q.front();
-    q.pop();
-    bfs.push_back(node);
+  for(int i=1;i<=V;i++){
+    if(!vis[i]){
+      queue < int > q;
+      q.push(0);
+      vis[0] = 1;
+      while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
 
-    for (auto it: adj[node]) {
-      if (!vis[it]) {
-        q.push(it);
-        vis[it] = 1;
+        for (auto it: adj[node]) {
+          if (!vis[it]) {
+            q.push(it);
+            vis[it] = 1;
+          }
+        }
       }
     }
   }
-
   return bfs;
 }
