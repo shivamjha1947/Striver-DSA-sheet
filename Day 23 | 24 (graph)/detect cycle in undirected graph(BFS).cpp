54757a48b5bea9bff9b57ea6c91f501cba4d1c65
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-08 12:55:38 +0530

    Update detect cycle in undirected graph(BFS).cpp

--- Day 23 | 24 (graph)/detect cycle in undirected graph(BFS).cpp
+++ Day 23 | 24 (graph)/detect cycle in undirected graph(BFS).cpp
@@ -1,34 +1,75 @@
- bool solve(vector<int> adj[], int vis[], int V, int c, int p)
+bool checkForCycle(int s, int V, vector<int> adj[], vector<int> &visited)
     {
-        vis[c]=1;
-        
-        for(auto i = adj[c].begin(); i!=adj[c].end(); i++)
+        // Create a queue for BFS
+        queue<pair<int, int>> q;
+        visited[s] = true;
+        q.push({s, -1});
+        while (!q.empty())
         {
-            if(!vis[*i])
+            int node = q.front().first;
+            int par = q.front().second;
+            q.pop();
+ 
+            for (auto it : adj[node])
             {
-                if(solve(adj,vis,V,*i,c))
-                    return true;
-            }
-            else
-            {
-                if(*i!=p)
-                    return true;
-            }
-        }
-        return false;
-    }
-    bool isCycle(int V, vector<int> adj[]) {
-        // Code here
-        int vis[V];
-        for(int i=0;i<V;i++)
-            vis[i]=0;
-        for(int i=0;i<V;i++)
-        {
-            if(!vis[i])
-                if(solve(adj,vis,V,i,-1)){
-                    return true;   
+                if (!visited[it])
+                {
+                    visited[it] = true;
+                    q.push({it, node});
                 }
+                else if (par != it)
+                    return true;
+            }
         }
         return false;
-         
     }
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> vis(V - 1, 0);
+        for (int i = 1; i <= V; i++)
+        {
+            if (!vis[i])
+            {
+                if (checkForCycle(i, V, adj, vis))
+                    return true;
+            }
+        }
+    } 
+
+
+
+
+// bool solve(vector<int> adj[], int vis[], int V, int c, int p)
+//     {
+//         vis[c]=1;
+        
+//         for(auto i = adj[c].begin(); i!=adj[c].end(); i++)
+//         {
+//             if(!vis[*i])
+//             {
+//                 if(solve(adj,vis,V,*i,c))
+//                     return true;
+//             }
+//             else
+//             {
+//                 if(*i!=p)
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+//     bool isCycle(int V, vector<int> adj[]) {
+//         // Code here
+//         int vis[V];
+//         for(int i=0;i<V;i++)
+//             vis[i]=0;
+//         for(int i=0;i<V;i++)
+//         {
+//             if(!vis[i])
+//                 if(solve(adj,vis,V,i,-1)){
+//                     return true;   
+//                 }
+//         }
+//         return false;
+         
+//     }
