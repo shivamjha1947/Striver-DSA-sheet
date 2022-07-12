68652066345f2125fc370d7bd0ab35a7e3f01c18
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-12 19:58:41 +0530

    Update (House Robber)Max Sum without Adjacents.cpp

--- Day 25 | 26 (Dynamic prog)/(House Robber)Max Sum without Adjacents.cpp
+++ Day 25 | 26 (Dynamic prog)/(House Robber)Max Sum without Adjacents.cpp
@@ -38,3 +38,19 @@ int solve(int *arr, int i,vector<int> &dp){
 	    vector<int> dp(n,-1);
 	    return solve(arr,n-1,dp);
 	}
+
+// For circular array
+int rob(vector<int>& nums) {
+        int n = nums.size(); 
+        if (n < 2) return n ? nums[0] : 0;
+        return max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
+    }
+    int robber(vector<int>& nums, int l, int r) {
+        int pre = 0, cur = 0;
+        for (int i = l; i <= r; i++) {
+            int temp = max(pre + nums[i], cur);
+            pre = cur;
+            cur = temp;
+        }
+        return cur;
+    }
