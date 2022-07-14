e59718d9a2dcd6eb82cb71757038f1f0f27022d7
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-14 17:26:04 +0530

    Create subset sum.cpp

---
+++ Day 25 | 26 (Dynamic prog)/subset sum.cpp
@@ -0,0 +1,52 @@
+// Memoize
+bool subsetSumUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+    if(target==0)
+        return true;
+    
+    if(ind == 0)
+        return arr[0] == target;
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    bool notTaken = subsetSumUtil(ind-1,target,arr,dp);
+    
+    bool taken = false;
+    if(arr[ind]<=target)
+        taken = subsetSumUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken||taken;
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr){
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    
+    return subsetSumUtil(n-1,k,arr,dp);
+}
+
+// Tabulation
+bool subsetSumToK(int n, int k, vector<int> &arr){
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    
+    return dp[n-1][k];
+}
