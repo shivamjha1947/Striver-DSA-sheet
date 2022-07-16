a97312b41bee6a5ff040bfc08d4ff5b086e1e770
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-16 19:32:40 +0530

    Create rod cutting.cpp

---
+++ Day 25 | 26 (Dynamic prog)/rod cutting.cpp
@@ -0,0 +1,53 @@
+// Memoization
+int cutRodUtil(vector<int>& price, int ind, int N, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        return N*price[0];
+    }
+    
+    if(dp[ind][N]!=-1)
+        return dp[ind][N];
+        
+    int notTaken = 0 + cutRodUtil(price,ind-1,N,dp);
+    
+    int taken = INT_MIN;
+    int rodLength = ind+1;
+    if(rodLength <= N)
+        taken = price[ind] + cutRodUtil(price,ind,N-rodLength,dp);
+        
+    return dp[ind][N] = max(notTaken,taken);
+}
+
+
+int cutRod(vector<int>& price,int N) {
+
+    vector<vector<int>> dp(N,vector<int>(N+1,-1));
+    return cutRodUtil(price,N-1,N,dp);
+}
+
+// Tabulation
+
+int cutRod(vector<int>& price,int N) {
+
+    vector<vector<int>> dp(N,vector<int>(N+1,-1));
+    
+    for(int i=0; i<=N; i++){
+        dp[0][i] = i*price[0];
+    }
+    
+    for(int ind=1; ind<N; ind++){
+        for(int length =0; length<=N; length++){
+        
+             int notTaken = 0 + dp[ind-1][length];
+    
+             int taken = INT_MIN;
+             int rodLength = ind+1;
+             if(rodLength <= length)
+                taken = price[ind] + dp[ind][length-rodLength];
+        
+             dp[ind][length] = max(notTaken,taken);   
+        }
+    }
+    
+    return dp[N-1][N];
+}
