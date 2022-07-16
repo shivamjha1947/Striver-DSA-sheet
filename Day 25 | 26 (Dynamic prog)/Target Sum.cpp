68d4a842aad8caecdc41d0c5569c68cccb4005cb
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-16 18:19:04 +0530

    Create Target Sum.cpp

---
+++ Day 25 | 26 (Dynamic prog)/Target Sum.cpp
@@ -0,0 +1,37 @@
+int find(vector<int> &num, int tar){
+         int n = num.size();
+
+        vector<vector<int>> dp(n,vector<int>(tar+1,0));
+
+        if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+        else dp[0][0] = 1;  // 1 case - not pick
+
+        if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 0; target<=tar; target++){
+
+                int notTaken = dp[ind-1][target];
+
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = dp[ind-1][target-num[ind]];
+
+                dp[ind][target]=notTaken + taken;
+            }
+        }
+        return dp[n-1][tar];
+    }
+
+    int findTargetSumWays(vector<int>& arr, int target){
+        int n=arr.size();
+        int totSum = 0;
+        for(int i=0; i<n;i++){
+            totSum += arr[i];
+        }
+
+        //Checking for edge cases
+        if(totSum-target <0 || (totSum-target)%2 ) return 0;
+
+        return find(arr,(totSum-target)/2);
+    }
