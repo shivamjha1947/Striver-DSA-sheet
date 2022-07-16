dac41babcb4770aa612f3552a5b9aa1bc248f995
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-16 18:53:01 +0530

    Update coin change 2.cpp

--- Day 25 | 26 (Dynamic prog)/coin change 2.cpp
+++ Day 25 | 26 (Dynamic prog)/coin change 2.cpp
@@ -23,3 +23,33 @@ long countWaysToMakeChange(vector<int>& arr, int n, int T){
     vector<vector<long>> dp(n,vector<long>(T+1,-1));
     return countWaysToMakeChangeUtil(arr,n-1, T, dp);
 }
+
+
+// Tabulation
+long countWaysToMakeChange(vector<int>& arr, int n, int T){
+    
+    vector<vector<long>> dp(n,vector<long>(T+1,0));
+    
+    
+    //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0)
+            dp[0][i]=1;
+        // Else condition is automatically fulfilled,
+        // as dp array is initialized to zero
+    }
+    
+    for(int ind=1; ind<n;ind++){
+        for(int target=0;target<=T;target++){
+            long notTaken = dp[ind-1][target];
+            
+            long taken = 0;
+            if(arr[ind]<=target)
+                taken = dp[ind][target-arr[ind]];
+                
+            dp[ind][target] = notTaken + taken;
+        }
+    }
+    
+    return dp[n-1][T];
+}
