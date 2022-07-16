b98bac0b5730ed43894d21e855edf982451adc5a
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-16 19:09:40 +0530

    Create unbounded knapsack.cpp

---
+++ Day 25 | 26 (Dynamic prog)/unbounded knapsack.cpp
@@ -0,0 +1,55 @@
+// Meemoize
+int knapsackUtil(vector<int>& wt, vector<int>& val, int ind, int W, vector<vector
+<int>>& dp){
+
+    if(ind == 0){
+        return ((int)(W/wt[0])) * val[0];
+    }
+    
+    if(dp[ind][W]!=-1)
+        return dp[ind][W];
+        
+    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+    
+    int taken = INT_MIN;
+    if(wt[ind] <= W)
+        taken = val[ind] + knapsackUtil(wt,val,ind,W-wt[ind],dp);
+        
+    return dp[ind][W] = max(notTaken,taken);
+}
+
+
+int unboundedKnapsack(int n, int W, vector<int>& val,vector<int>& wt) {
+    
+    vector<vector<int>> dp(n,vector<int>(W+1,-1));
+    return knapsackUtil(wt, val, n-1, W, dp);
+}
+
+
+// Tabulation
+
+int unboundedKnapsack(int n, int W, vector<int>& val,vector<int>& wt) {
+    
+    vector<vector<int>> dp(n,vector<int>(W+1,0));
+    
+    //Base Condition
+    
+    for(int i=wt[0]; i<=W; i++){
+        dp[0][i] = ((int) i/wt[0]) * val[0];
+    }
+    
+    for(int ind =1; ind<n; ind++){
+        for(int cap=0; cap<=W; cap++){
+            
+            int notTaken = 0 + dp[ind-1][cap];
+            
+            int taken = INT_MIN;
+            if(wt[ind] <= cap)
+                taken = val[ind] + dp[ind][cap - wt[ind]];
+                
+            dp[ind][cap] = max(notTaken, taken);
+        }
+    }
+    
+    return dp[n-1][W];
+}
