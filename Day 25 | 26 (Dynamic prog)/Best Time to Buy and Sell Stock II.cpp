4a3f13a37e575ac745da1fd1eaac7455f8124e7f
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-19 16:57:15 +0530

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Day 25 | 26 (Dynamic prog)/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,6 @@
+int maxProfit(vector<int> &prices) {
+        int ret = 0;
+        for (int p = 1; p < prices.size(); ++p) 
+          ret += max(prices[p] - prices[p - 1], 0);    
+        return ret;
+    }
