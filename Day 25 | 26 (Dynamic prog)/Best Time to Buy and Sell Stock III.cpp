74d4e0516f3f34bf96d2e4d2113916825f3ffad7
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-19 21:45:07 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- Day 25 | 26 (Dynamic prog)/Best Time to Buy and Sell Stock III.cpp
+++ Day 25 | 26 (Dynamic prog)/Best Time to Buy and Sell Stock III.cpp
@@ -1,3 +1,19 @@
+// Best and fastest
+int maxProfit(vector<int>& prices)  {
+        int buy1 = INT_MAX, buy2 = INT_MAX;
+        int sell1 = 0, sell2 = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            buy1 = min(buy1, prices[i]);
+            sell1 = max(sell1, prices[i] - buy1);
+            buy2 = min(buy2, prices[i] - sell1);
+            sell2 = max(sell2, prices[i] - buy2);
+        }
+
+        return sell2;
+    }
+
+
 // Best Space optimized
 
 int maxProfit(vector<int>& Arr, int n)
