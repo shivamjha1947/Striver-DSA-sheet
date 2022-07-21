487176cfdbe3e14075463036bc6a14834d6c62b4
shivam jha <shivam36_soe@jnu.ac.in>
2022-07-21 23:38:19 +0530

    Update Longest increasing subsequence.cpp

--- Day 25 | 26 (Dynamic prog)/Longest increasing subsequence.cpp
+++ Day 25 | 26 (Dynamic prog)/Longest increasing subsequence.cpp
@@ -27,3 +27,50 @@ int longestIncreasingSubsequence(int arr[], int n){
     return getAns(arr,n,0,-1,dp);
 }
 
+// Tabulation
+int longestIncreasingSubsequence(int arr[], int n){
+    
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    
+    for(int ind = n-1; ind>=0; ind --){
+        for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+            
+            int notTake = 0 + dp[ind+1][prev_index +1];
+    
+            int take = 0;
+    
+            if(prev_index == -1 || arr[ind] > arr[prev_index]){
+                
+                take = 1 + dp[ind+1][ind+1];
+            }
+    
+            dp[ind][prev_index+1] = max(notTake,take);
+            
+        }
+    }
+    
+    return dp[0][0];
+}
+
+// Another approach
+int longestIncreasingSubsequence(int arr[], int n){
+    
+    vector<int> dp(n,1);
+    
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i]){
+                dp[i] = max(dp[i], 1 + dp[prev_index]);
+            }
+        }
+    }
+    
+    int ans = -1;
+    
+    for(int i=0; i<=n-1; i++){
+        ans = max(ans, dp[i]);
+    }
+    
+    return ans;
+}
